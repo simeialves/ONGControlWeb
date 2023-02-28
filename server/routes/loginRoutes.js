0f6a78c6bf86ec4a90054c966301f03196f22234
simeialves <simeiparreiras@gmail.com>
2023-02-28 17:09:21 -0300

    master

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -2,50 +2,50 @@ require("dotenv").config();
 const express = require("express");
 const db = require("../config/db");
 const loginRoutes = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-loginRoutes.post('/loginAuth', (req, res) => {
+loginRoutes.post("/loginAuth", (req, res) => {
   db.knex
-    .select('*').from('usuario').where({ login: req.body.email })
-    .then(async usuarios => {
+    .select("*")
+    .from("usuario")
+    .where({ login: req.body.email })
+    .then(async (usuarios) => {
       if (usuarios.length) {
-        let usuario = usuarios[0]
-        let checkSenha = await bcrypt.compare(req.body.password, usuario.SENHA)
+        let usuario = usuarios[0];
+        let checkSenha = await bcrypt.compare(req.body.password, usuario.senha);
         if (checkSenha) {
-          var tokenJWT = jwt.sign({ id: usuario.USUARIOID },
-            process.env.SECRET_KEY, {
-            expiresIn: 3600
-          })
-          res.status(200).json(
+          var tokenJWT = jwt.sign(
+            { id: usuario.usuarioid },
+            process.env.SECRET_KEY,
             {
-              success: true,
-              message: 'Logado com sucesso',
-              id: usuario.USUARIOID,
-              email: usuario.LOGIN,
-              access_token: tokenJWT
+              expiresIn: 3600,
             }
-          )
-          return
-        }
-        else
+          );
+          res.status(200).json({
+            success: true,
+            message: "Logado com sucesso",
+            id: usuario.usuarioid,
+            email: usuario.login,
+            access_token: tokenJWT,
+          });
+          return;
+        } else
           res.status(401).json({
             success: false,
-            message: 'Login ou senha incorretos.'
-          })
-      }
-      else
+            message: "Login ou senha incorretos.",
+          });
+      } else
         res.status(401).json({
           success: false,
-          message: 'Login ou senha incorretos.'
-        })
-
+          message: "Login ou senha incorretos.",
+        });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
-        message: 'Erro ao verificar login - ' + err.message
-      })
-    })
+        message: "Erro ao verificar login - " + err.message,
+      });
+    });
 });
 
 module.exports = loginRoutes;
--- server/routes/usuarioRoutes.js
+++ server/routes/usuarioRoutes.js
@@ -1,54 +1,56 @@
 const express = require("express");
 const db = require("../config/db");
 const usuarioRoutes = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function verifyJWT(req, res, next) {
-    var token = req.headers["x-access-token"];
-  
-    if (!token) {
-      return res.status(401).send({ auth: false, message: "No token provided." });
-    }
-  
-    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
-      if (err)
-        return res
-          .status(500)
-          .send({ auth: false, message: "Failed to authenticate token." });
-  
-      // se tudo estiver ok, salva no request para uso posterior
-      req.userId = decoded.id;
-      next();
-    });
+  var token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(401).send({ auth: false, message: "No token provided." });
   }
 
-usuarioRoutes.post('/register', verifyJWT, (req, res, next) => {
-    const { nome, login, senha, senha2 } = req.body
+  jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate token." });
 
-    if (senha != senha2) {
-        res.status(200).json("As senhas não conferem")
-    }
-    else {
-        db.knex('usuario')
-            .insert({
-                login: login,
-                senha: bcrypt.hashSync(senha, 8),
-                ativo: 1,
-                tipo: 2
-            }, ['usuarioid'])
-            .then((result) => {
-                let usuario = result[0]
-                res.status(200).json({ "usuarioid": usuario.usuarioid })
-                return
-            })
-            .catch(err => {
-                res.status(500).json({
-                    message: 'Erro ao registrar usuario - ' + err.message
-                })
-            })
-    }
-})
+    // se tudo estiver ok, salva no request para uso posterior
+    req.userId = decoded.id;
+    next();
+  });
+}
+
+usuarioRoutes.post("/register", (req, res, next) => {
+  const { nome, login, senha, senha2 } = req.body;
+
+  if (senha != senha2) {
+    res.status(200).json("As senhas não conferem");
+  } else {
+    db.knex("usuario")
+      .insert(
+        {
+          login: login,
+          senha: bcrypt.hashSync(senha, 8),
+          ativo: 1,
+          tipo: 2,
+        },
+        ["usuarioid"]
+      )
+      .then((result) => {
+        let usuario = result[0];
+        res.status(200).json({ usuarioid: usuario.usuarioid });
+        return;
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Erro ao registrar usuario - " + err.message,
+        });
+      });
+  }
+});
 
 module.exports = usuarioRoutes;
