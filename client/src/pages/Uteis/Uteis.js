b0901d7fe377879b4bea5283cfa5daf67d99beb1
Simei Alves <simeiparreiras@gmail.com>
2023-03-13 19:55:13 -0300

    master

--- client/src/pages/Uteis/Uteis.js
+++ client/src/pages/Uteis/Uteis.js
@@ -1,3 +1,14 @@
 export function formatDate(dataInput) {
   return new Date(dataInput).toLocaleString().replace(", 00:00:00", "");
 }
+
+export function formateDateNoTime(dateInput) {
+  let data = new Date(dateInput),
+    dia = data.getDate().toString(),
+    diaF = dia.length == 1 ? "0" + dia : dia,
+    mes = (data.getMonth() + 1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+    mesF = mes.length == 1 ? "0" + mes : mes,
+    anoF = data.getFullYear();
+
+  return anoF + "-" + mesF + "-" + diaF;
+}
