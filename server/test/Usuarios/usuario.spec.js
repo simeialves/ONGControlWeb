a363bbed7007d254036131000da5faf68a9b6526
Simei Alves <simeiparreiras@gmail.com>
2023-04-08 16:30:11 -0300

    master

--- server/test/Usuarios/usuario.spec.js
+++ server/test/Usuarios/usuario.spec.js
@@ -1,6 +1,6 @@
 describe("Testando conexão com o banco de dados", () => {
   test("Conectando ao banco de dados", async () => {
-    const [rows] = await global.connection.execute("SELECT 1 + 1 AS solution");
+    const [rows] = await global.connection.execute("SELECT * FROM usuario");
     expect(rows[0].solution).toBe(2);
   });
 });
