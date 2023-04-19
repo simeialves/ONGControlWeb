1a9126cf6a9a38acb85f3dfe2b1dfa327f89e10d
simeialves <simeiparreiras@gmail.com>
2023-04-19 10:08:56 -0300

    master

--- client/src/pages/HomePage/card.js
+++ client/src/pages/HomePage/card.js
@@ -64,8 +64,8 @@ export const CardEvento = () => {
               >
                 <CardBody>
                   <Image
-                    src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                    alt="Green double couch with wooden legs"
+                    src="https://simeialves.com.br/images/natalparatodos.jpg"
+                    alt="Natal para Todos"
                     borderRadius="lg"
                   />
                   <Stack mt="6" spacing="3">
