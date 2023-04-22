ad626ac9548588f4141e1e9a77bfe44739bdfe47
Simei Alves <simeiparreiras@gmail.com>
2023-04-22 10:54:16 -0300

    master

--- client/src/shared/services/api.js
+++ client/src/shared/services/api.js
@@ -4,7 +4,7 @@ import { removeCaracter } from "../../pages/Uteis/Uteis";
 export const token = localStorage.getItem("access_token");
 
 export const api = axios.create({
-  baseURL: "http://35.169.154.37/",
+  baseURL: "http://54.163.168.191/",
   headers: { "x-access-token": token },
 });
 
