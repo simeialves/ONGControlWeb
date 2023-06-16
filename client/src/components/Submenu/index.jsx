3596452031375d35de75a8c6d22c388a9b170e4a
Simei Alves <simeiparreiras@gmail.com>
2023-06-16 16:44:33 -0300

    master

---
+++ client/src/components/Submenu/index.jsx
@@ -0,0 +1,14 @@
+class ListaSubmenu {
+  static eventos = [
+    {
+      id: 1,
+      descricao: "Eventos",
+    },
+    {
+      id: 2,
+      descricao: "Local dos Eventos",
+    },
+  ];
+}
+
+export default ListaSubmenu;
