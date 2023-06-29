0ff8e0431fd460ebd5b5ffd5b08b3e6281da115d
Simei Alves <simeiparreiras@gmail.com>
2023-06-28 21:48:33 -0300

    master

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.status(200).json("Página Inicial do ONGControlWeb - DevOps");
+  return res.status(200).json("Página Inicial do ONGControlWeb");
 });
 
 app.get("/ping", (req, res) => {
